74cd786dec591fac4aa493217d8eeed4c30df9fd
LuizEscobarC <luiz_escobar11@hotmail.com>
2025-05-23 19:42:41 -0400

    feat: langs pt br

---
+++ lang/pt/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'welcome' => 'Bem-vindo à nossa aplicação!',
+    'goodbye' => 'Até logo!',
+    'app_name' => 'Minha Aplicação Laravel',
+
+    // Termos comuns
+    'actions' => 'Ações',
+    'save' => 'Salvar',
+    'edit' => 'Editar',
+    'delete' => 'Excluir',
+    'cancel' => 'Cancelar',
+    'confirm' => 'Confirmar',
+    'back' => 'Voltar',
+    'submit' => 'Enviar',
+
+    // Termos específicos da aplicação
+    'users' => 'Usuários',
+    'job_listings' => 'Vagas de Emprego',
+    'job_applications' => 'Candidaturas',
+    'profile' => 'Perfil',
+    'dashboard' => 'Painel',
+];
