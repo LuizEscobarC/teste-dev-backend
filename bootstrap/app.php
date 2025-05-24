93c186de4a6ec66a43ff4433b29e877495717093
LuizEscobarC <luiz_escobar11@hotmail.com>
2025-05-24 04:54:33 -0400

    refactor: exceptions info app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,17 +22,49 @@ return Application::configure(basePath: dirname(__DIR__))
             // 
         ]);
         
-        $exceptions->reportable(function (Throwable $e) {
-            //
-        });
-        
-        $exceptions->renderable(function (ValidationException $e, $request) {
-            if ($request->expectsJson() || $request->is('api/*')) {
-                return response()->json([
-                    'message' => $e->getMessage(),
-                    'errors' => $e->errors(),
-                ], $e->status);
+        $exceptions->renderable(function (Throwable $e, $request) {
+            if ($e->getPrevious() instanceof ValidationException) {
+                if ($request->expectsJson() || $request->is('api/*')) {
+                    return response()->json([
+                        'message' => $e->getMessage(),
+                        'errors' => $e->errors(),
+                    ], $e->status);
+                }
+                return null;
             }
+
+            if ($e->getPrevious() instanceof ModelNotFoundException) {
+                if ($request->expectsJson() || $request->is('api/*')) {
+                    return response()->json([
+                        'message' => 'Registro não encontrado.',
+                        'error' => 'not_found'
+                    ], 404);
+                }
+                return null;
+            }
+            if ($e->getPrevious() instanceof \Illuminate\Database\QueryException) {
+                if ($request->expectsJson() || $request->is('api/*')) {
+                    return response()->json([
+                        'message' => 'Erro de consulta ao banco de dados.',
+                        'error' => 'query_exception'
+                    ], 500);
+                }
+                return null;
+            }
+
+            if ($e->getPrevious() instanceof TypeError) {
+                if (($request->expectsJson() || $request->is('api/*')) && 
+                    str_contains($e->getMessage(), 'must be of type') && 
+                    str_contains($e->getMessage(), 'none returned')) {
+                    return response()->json([
+                        'message' => $e->getMessage(),
+                        'error' => $e->getTraceAsString(),
+                    ], 404);
+                }
+                return null;
+            }
+
             return null;
         });
+
     })->create();
